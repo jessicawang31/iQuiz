786600d47fe5b3ad73b5b748f0480f1426033ad0
Jessica Wang <jessicaw.040131@gmail.com>
2025-05-05 19:27:29 -0700

    almost done

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -2,36 +2,27 @@
 //  ViewController.swift
 //  iQuiz
 //
-//  Created by Jessica  Wang on 5/5/25.
+//  Created by Jessica Wang on 5/5/25.
 //
 
 import UIKit
 
-// Custom cell class
 class QuizCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var iconImageView: UIImageView!
 }
 
-// Data model
-struct QuizTopic {
-    let iconName: String
-    let title: String
-    let description: String
-}
-
-// Main view controller
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var settingsButton: UIBarButtonItem!
     
-    // In-memory quiz topics
-    let quizTopics: [QuizTopic] = [
-        QuizTopic(iconName: "Math", title: "Mathematics", description: "Did you pass the third grade?"),
-        QuizTopic(iconName: "Marvel", title: "Marvel Super Heroes", description: "Avengers, Assemble!"),
-        QuizTopic(iconName: "Science", title: "Science!", description: "Because SCIENCE!")
+    // Use a simple tuple array just like the working code
+    let quizTopics = [
+        ("Mathematics", "How are you math skills?"),
+        ("Science", "Ready to put your knowledge to the test?"),
+        ("Marvel Super Heroes", "Are you a real Marvel fan?")
     ]
     
     var selectedTopic: String = "Marvel Super Heroes"
@@ -42,30 +33,39 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
     }
     
-    // MARK: - TableView DataSource
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return quizTopics.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "QuizCell", for: indexPath) as! QuizCell
         let topic = quizTopics[indexPath.row]
         
-        cell.titleLabel.text = topic.title
-        cell.descriptionLabel.text = topic.description
-        cell.iconImageView.image = UIImage(named: topic.iconName)
+        cell.titleLabel.text = topic.0
+        cell.descriptionLabel.text = topic.1
+        
+        let imgName: String
+        switch topic.0 {
+        case "Mathematics":
+            imgName = "Math"
+        case "Science":
+            imgName = "Science"
+        case "Marvel Super Heroes":
+            imgName = "Marvel"
+        default:
+            imgName = ""
+        }
+        cell.iconImageView.image = UIImage(named: imgName)
         
         return cell
     }
     
-    // MARK: - TableView Delegate
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedTopic = quizTopics[indexPath.row].title
+        selectedTopic = quizTopics[indexPath.row].0
         performSegue(withIdentifier: "setQuestion", sender: self)
     }
-    
+//    
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //        if segue.identifier == "setQuestion",
 //           let destinationVC = segue.destination as? Quiz_ViewController {
